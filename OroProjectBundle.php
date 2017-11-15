cf26dd22a05f333718c50106a57c16a0f815b9e6
Egor Zyuskin <ezyuskin@codenetix.com>
2017-11-15 09:11:01 +0600

    Add OroProjectBundle

---
+++ OroProjectBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Codenetix\Oro\Bundle\ProjectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Egor Zyuskin <ezyuskin@codenetix.com>
+ */
+class OroProjectBundle extends Bundle
+{
+}
